c6faf165d324f7093a352f5ca3bd134af231dfe7
Nicolas Jaccard <nicolas.jaccard@gmail.com>
2021-02-08 16:12:19 +0000

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -112,7 +112,7 @@ function App() {
                 <a href="https://github.com/nicjac/vaccine-tracker-uk">
                   Github
                   <Icon name="github" />
-                  respository!
+                  repository!
                 </a>
               </List.Item>
             </List>
