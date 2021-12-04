87fc0997c6d5d19d795d0f160a14d71e690dc794
Nicolas Jaccard <nicolas.jaccard@gmail.com>
2021-12-04 16:01:41 +0000

    Added temporary offline message

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  Container,
+  Message,
+} from "semantic-ui-react";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Container>
+    <Message info style={{marginTop:"200px"}}>
+          <Message.Header>We will be back! </Message.Header>
+          <p>
+            This website has not been updated for some time due to technical difficulties. Instead of showing outdated information, we decied to take it offline until we get the time to resolve these issues. Until then,
+            please visit the official <a href="https://coronavirus.data.gov.uk/">UK COVID dashboard</a> for updates. Be safe!
+          </p>
+        </Message>
+        </Container>
+      {/* <App /> */}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
